d9436ca98b803b15f2dd5a40608eea3a109dc0f7
Martin Peña <el.martin.pena@gmail.com>
2026-08-11 11:31:06 -0400

    Fix 403 error on Dashboard by adding verifyToken

--- backend/src/routes/home.routes.ts
+++ backend/src/routes/home.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getHomeDashboard, addGrowthRecord } from "../controllers/home.controller";
+import { verifyToken } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 // Endpoint for Home Dashboard
-router.get("/:idPerfil", getHomeDashboard);
-router.post("/:idPerfil/crecimiento", addGrowthRecord);
+router.get("/:idPerfil", verifyToken, getHomeDashboard);
+router.post("/:idPerfil/crecimiento", verifyToken, addGrowthRecord);
 
 export default router;
